f2577fd5dc8023364e59cc9668295028265253d2
Yannick Chevalier <yannick.chevalier@irit.fr>
2017-10-24 18:47:47 +0200

    ajout des correction

---
+++ TP01/tri_entiers.c
@@ -0,0 +1,41 @@
+
+
+
+void tri_a_bulles ( int * t , int n )
+{
+  // Écrire une fonctions prenant en entrée un tableau d'entier,
+  // et triant les éléments de ce tableau
+
+  
+
+}
+
+
+
+void fusion ( int * res , int * t , int l1 , int l2 )
+{
+  /*
+    Écrire une procédure fusion qui prend en entrée:
+    - deux tableaux de même taille res et t
+    - deux entiers l1 et l2 tels que:
+    * t[0],...,t[l1-1] est trié
+    * t[l1],...,t[l1+l2 -1] est trié
+    et qui met dans res[0],...,res[l1+l2-1] les éléments
+    t[0],...,t[l1+l2-1] dans l'ordre croissant
+   */
+}
+
+void recopie ( int * res , int * t , int n )
+{
+  /*
+    Écrire une procédure qui copie les n premiers éléments du tableau
+    t dans le tableau res
+   */
+  
+}
+
+void tri_fusion ( int * t , int n )
+{
+  
+  
+}
---
+++ include/entrees.h
@@ -0,0 +1,16 @@
+
+int demande_decimal ( float * n ) ;
+
+int demande_entier ( int * n ) ;
+
+int demande_entier_positif ( int * n ) ;
+ 
+int
+demande_decimal_long ( double * f ) ;
+int
+demande_mot ( char ** s ) ;
+
+int
+demande_ligne ( char ** s ) ;
+
+
